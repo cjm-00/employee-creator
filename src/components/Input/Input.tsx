457317c5d7b57293a4db95de3ca9538912ad0c9e
cjm-00 <cearajoanmckenna@gmail.com>
2025-03-24 11:43:48 +1100

    added name and register as props for input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,18 +1,38 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import classes from "./Input.module.scss";
+import { IFormInput } from "../../pages/CreatePage/CreatePage";
 
 interface inputProps {
   variant?: "primary" | "secondary";
   label?: string;
+  name:
+    | "firstname"
+    | "surname"
+    | "dob"
+    | "gender"
+    | "email"
+    | "phone"
+    | "address"
+    | "contractType"
+    | "startDate"
+    | "endDate"
+    | "jobTitle";
+  register?;
 }
 
 /// https://react-hook-form.com/get-started
 
-export default function Input({ variant = "primary", label }: inputProps) {
+export default function Input({
+  variant = "primary",
+  label,
+  name,
+  register,
+}: inputProps) {
   return (
     <>
       <div className={`${classes.inputWrap} ${classes[variant]}`}>
         <label className={classes.label}>{label}</label>
-        <input className={classes.input} />
+        <input {...register(name)} className={classes.input} />
       </div>
     </>
   );
