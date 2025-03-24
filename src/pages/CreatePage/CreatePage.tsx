e65b0bb3ce78b6fc9157b6c539ccc9a76ab3940c
cjm-00 <cearajoanmckenna@gmail.com>
2025-03-24 23:59:00 +1100

    updated styling, updated input types and added validation

--- src/pages/CreatePage/CreatePage.tsx
+++ src/pages/CreatePage/CreatePage.tsx
@@ -5,6 +5,9 @@ import Header from "../../components/Header/Header";
 import Input from "../../components/Input/Input";
 import classes from "./CreatePage.module.scss";
 import { createEmployee } from "../../services/employee-services";
+import { useNavigate } from "react-router";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import Select from "../../components/Select/Select";
 
 export interface IFormInput {
   firstname: string;
@@ -20,8 +23,34 @@ export interface IFormInput {
   jobTitle: string;
 }
 export default function CreatePage() {
-  const { handleSubmit, register } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => createEmployee(data);
+  let navigate = useNavigate();
+  let queryClient = useQueryClient();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    mutation.mutate(data);
+  };
+
+  const mutation = useMutation({
+    mutationFn: (data: IFormInput) => createEmployee(data),
+    onError: (error) => {
+      console.log(`error on creation:`, error);
+    },
+    onSuccess: () => {
+      console.log("successful create bro");
+      queryClient.invalidateQueries({
+        queryKey: ["employees"],
+        refetchType: "active",
+      });
+      navigate(`/employees`);
+    },
+  });
+
+  const genderOptions = ["Female", "Male", "Other", "Na"];
 
   return (
     <>
@@ -35,31 +64,123 @@ export default function CreatePage() {
         >
           <div className={classes.formSection}>
             <h2 className={classes.sectionHeading}>Personal Details</h2>
-            <Input label="First Name" name="firstname" register={register} />
-            <Input label="Last Name" register={register} name={"surname"} />
-            <Input label="Date of Birth" register={register} name={"dob"} />
-            <Input label="Gender" register={register} name={"gender"} />
+            <Input
+              required
+              label="First Name"
+              name="firstname"
+              register={register}
+              min={2}
+              max={20}
+              error={errors.firstname?.message}
+            />
+            <Input
+              required
+              label="Last Name"
+              register={register}
+              name={"surname"}
+              min={2}
+              max={30}
+              error={errors.surname?.message}
+            />
+            <Input
+              required
+              label="Date of Birth"
+              register={register}
+              name={"dob"}
+              type="date"
+              error={errors.dob?.message}
+            />
+            <Select
+              name={"gender"}
+              label="Gender"
+              register={register}
+              options={genderOptions}
+              error={errors.gender?.message}
+              required
+            />
           </div>
 
           <div className={classes.formSection}>
             <h2 className={classes.sectionHeading}>Contact Details</h2>
-            <Input label="Email" name={"email"} register={register} />
-            <Input label="Phone Number" name={"phone"} register={register} />
-            <Input label="Address" name={"address"} register={register} />
+            <Input
+              required
+              label="Email"
+              name={"email"}
+              type="email"
+              register={register}
+              pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+              error={errors.email?.message}
+            />
+            <Input
+              label="Phone Number (0000-000-000)"
+              type="tel"
+              name={"phone"}
+              register={register}
+              placeholder="04XX-XXX-XXX"
+              pattern="^[0-9]{4}-[0-9]{3}-[0-9]{3}$"
+              required
+              error={errors.phone?.message}
+            />
+            <Input
+              required
+              label="Street Address (123 Test Street)"
+              name={"address"}
+              register={register}
+              pattern="^(\d+) ?([A-Za-z](?= ))? (.*?) ([^ ]+?) ?((?<= )APT)? ?((?<= )\d*)?$"
+              error={errors.address?.message}
+            />
           </div>
 
           <div className={classes.formSection}>
             <h2 className={classes.sectionHeading}>Employment Details</h2>
-            <Input
-              label="Contract Type"
-              name={"contractType"}
-              register={register}
-            />
-            <Input label="Start Date" name={"startDate"} register={register} />
-            <Input label="End Date" name={"endDate"} register={register} />
-            <Input label="Job Title" name={"jobTitle"} register={register} />
-          </div>
+            <div className={classes.contractTypeDiv}>
+              <Input
+                variant="radio"
+                type="radio"
+                required
+                label="Full-Time"
+                name={"contractType"}
+                value="FT"
+                register={register}
+                error={errors.contractType?.message}
+              />
+              <Input
+                variant="radio"
+                type="radio"
+                required
+                label="Part-Time"
+                name={"contractType"}
+                value="PT"
+                register={register}
+                error={errors.contractType?.message}
+              />
+            </div>
 
+            <Input
+              required
+              label="Start Date"
+              type="date"
+              name={"startDate"}
+              register={register}
+              error={errors.startDate?.message}
+            />
+            <Input
+              label="End Date"
+              type="date"
+              name={"endDate"}
+              register={register}
+              error={errors.endDate?.message}
+            />
+            <Input
+              required
+              label="Job Title"
+              name={"jobTitle"}
+              register={register}
+              error={errors.jobTitle?.message}
+              min={2}
+              max={35}
+            />
+          </div>
           <div className={classes.btnContainer}>
             <Button variant="primary" asLink linkTo="/">
               Cancel
@@ -69,6 +190,7 @@ export default function CreatePage() {
             </Button>
           </div>
         </form>
+
         <Footer />
       </div>
     </>
--- src/pages/EditPage/EditPage.tsx
+++ src/pages/EditPage/EditPage.tsx
@@ -9,7 +9,8 @@ import {
   updateEmployee,
 } from "../../services/employee-services";
 import { useNavigate, useParams } from "react-router";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import Select from "../../components/Select/Select";
 
 export interface IFormInput {
   firstname: string;
@@ -27,22 +28,40 @@ export interface IFormInput {
 export default function EditPage() {
   const { id } = useParams();
   let navigate = useNavigate();
+  let queryClient = useQueryClient();
   const employeeQueryInfo = useQuery({
     queryKey: ["employee", id],
     queryFn: () => getEmployeeById(Number(id)),
   });
 
-  const { handleSubmit, register } = useForm<IFormInput>({
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<IFormInput>({
     defaultValues: employeeQueryInfo.data,
   });
+
+  const mutation = useMutation({
+    mutationFn: (data: IFormInput) => updateEmployee(Number(id), data),
+    onError: (error) => {
+      console.log(`error on update:`, error);
+    },
+    onSuccess: () => {
+      console.log("successful update bro");
+      queryClient.invalidateQueries({
+        queryKey: ["employees", "employee"],
+        refetchType: "active",
+      });
+      navigate(`/employees/${id}`);
+    },
+  });
+
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    updateEmployee(Number(id), data);
-    redirect();
+    mutation.mutate(data);
   };
 
-  const redirect = () => {
-    navigate(`/employees/${id}`);
-  };
+  const genderOptions = ["Female", "Male", "Other", "Na"];
 
   return (
     <>
@@ -56,31 +75,123 @@ export default function EditPage() {
         >
           <div className={classes.formSection}>
             <h2 className={classes.sectionHeading}>Personal Details</h2>
-            <Input label="First Name" name="firstname" register={register} />
-            <Input label="Last Name" register={register} name={"surname"} />
-            <Input label="Date of Birth" register={register} name={"dob"} />
-            <Input label="Gender" register={register} name={"gender"} />
+            <Input
+              required
+              label="First Name"
+              name="firstname"
+              register={register}
+              min={2}
+              max={20}
+              error={errors.firstname?.message}
+            />
+            <Input
+              required
+              label="Last Name"
+              register={register}
+              name={"surname"}
+              min={2}
+              max={30}
+              error={errors.surname?.message}
+            />
+            <Input
+              required
+              label="Date of Birth"
+              register={register}
+              name={"dob"}
+              type="date"
+              error={errors.dob?.message}
+            />
+            <Select
+              name={"gender"}
+              label="Gender"
+              register={register}
+              options={genderOptions}
+              error={errors.gender?.message}
+              required
+            />
           </div>
 
           <div className={classes.formSection}>
             <h2 className={classes.sectionHeading}>Contact Details</h2>
-            <Input label="Email" name={"email"} register={register} />
-            <Input label="Phone Number" name={"phone"} register={register} />
-            <Input label="Address" name={"address"} register={register} />
+            <Input
+              required
+              label="Email"
+              name={"email"}
+              type="email"
+              register={register}
+              pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+              error={errors.email?.message}
+            />
+            <Input
+              label="Phone Number (0000-000-000)"
+              type="tel"
+              name={"phone"}
+              register={register}
+              placeholder="04XX-XXX-XXX"
+              pattern="^[0-9]{4}-[0-9]{3}-[0-9]{3}$"
+              required
+              error={errors.phone?.message}
+            />
+            <Input
+              required
+              label="Street Address (123 Test Street)"
+              name={"address"}
+              register={register}
+              pattern="^(\d+) ?([A-Za-z](?= ))? (.*?) ([^ ]+?) ?((?<= )APT)? ?((?<= )\d*)?$"
+              error={errors.address?.message}
+            />
           </div>
 
           <div className={classes.formSection}>
             <h2 className={classes.sectionHeading}>Employment Details</h2>
-            <Input
-              label="Contract Type"
-              name={"contractType"}
-              register={register}
-            />
-            <Input label="Start Date" name={"startDate"} register={register} />
-            <Input label="End Date" name={"endDate"} register={register} />
-            <Input label="Job Title" name={"jobTitle"} register={register} />
-          </div>
+            <div className={classes.contractTypeDiv}>
+              <Input
+                variant="radio"
+                type="radio"
+                required
+                label="Full-Time"
+                name={"contractType"}
+                value="FT"
+                register={register}
+                error={errors.contractType?.message}
+              />
+              <Input
+                variant="radio"
+                type="radio"
+                required
+                label="Part-Time"
+                name={"contractType"}
+                value="PT"
+                register={register}
+                error={errors.contractType?.message}
+              />
+            </div>
 
+            <Input
+              required
+              label="Start Date"
+              type="date"
+              name={"startDate"}
+              register={register}
+              error={errors.startDate?.message}
+            />
+            <Input
+              label="End Date"
+              type="date"
+              name={"endDate"}
+              register={register}
+              error={errors.endDate?.message}
+            />
+            <Input
+              required
+              label="Job Title"
+              name={"jobTitle"}
+              register={register}
+              error={errors.jobTitle?.message}
+              min={2}
+              max={35}
+            />
+          </div>
           <div className={classes.btnContainer}>
             <Button variant="primary" asLink linkTo={`/employees/${id}`}>
               Cancel
@@ -90,6 +201,7 @@ export default function EditPage() {
             </Button>
           </div>
         </form>
+
         <Footer />
       </div>
     </>
